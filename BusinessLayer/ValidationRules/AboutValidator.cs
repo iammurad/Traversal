ad704e8b6aa705dd5611004f694f44fb7ba82931
LinuxAlien <murik8900@gmail.com>
2022-04-26 02:11:43 +0400

    crud  & FrontSide

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+   public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Please fill the Description");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Please pick any photo");
+        }
+    }
+}
---
+++ Traversal/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccesLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Traversal.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
